51463d708297bde8d327a75f9b7bb763ca4757cf
Tegar <tegar20ixc@gmail.com>
2023-07-11 17:15:26 +0700

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by I Nyoman Tegar Pramudya on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
